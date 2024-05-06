c53f7903ac45a073dfa3816319e04ce61ae783a6
Dunne-Andrew <71160402+Dunne-Andrew@users.noreply.github.com>
2024-05-05 22:24:28 -0600

    Add files via upload

--- utilities.asm
+++ utilities.asm
@@ -7,23 +7,22 @@ extern  _ReadConsoleA@20:near
 
 
 .data
+
 outputHandle		dword   ?						; Storage the the handle for input and output. uninitslized
 inputHandle         dword   ?
-numCharsRead        dword 1
 readBuffer byte 1024 dup(00h)
 writeBuffer byte 1024 dup(00h)
+numCharsRead        dword 1
 count   dword   1
-wBIndex  dword ?
 countDown dword 1
+wBIndex  dword ?
 written				dword   ?
-prompt            byte        'Input a number n: '
 results           byte        "n = "
 	
 .code
-initUtilities PROC near
+
+initUtilities PROC near  ; This is the initializer function 
 _initUtilities:
-
-
 
 
 	push    -11
@@ -37,60 +36,7 @@ _initUtilities:
 
 initUtilities ENDP
 
-inputToInteger PROC near 
-; Some of you asked for my code to convert string to integer. I ; just built it into my main routine. It should be broken out into ; its own function. That would be a good exercise for you.
-_rereadMaxNumber:
-    ; Type a prompt for the user
-    ; WriteConsole(handle, &Prompt[0], 18, &written, 0)
-    push  0
-    push  offset written
-    push  18
-    push  offset prompt
-    mov   eax, offset prompt
-    push  outputHandle
-    call  _WriteConsoleA@20
-    ; handle = GetStdHandle(-10)
-    push  -10
-    call  _GetStdHandle@4
-    mov   inputHandle, eax
-    call  readline
-    ; The following embeds the above code in a common routine, so the more complicated call only needs to be written once.
-    ; writeline(&results[0], 12)
-    push  4
-    push  offset results
-    call  writeline
-    ; writeline(&readBuffer[0], numCharsToWrite - aka numCharsRead)
-    push  numCharsRead
-    push  offset readBuffer
-    call  writeline
-    ; Take what was read and convert to a number
-    mov   eax, 0               ; Initialize the number
-    mov   ecx, 0               ; Initialize index into buffer
-    mov   ebx, 0               ; Make sure upper bits are all zero.
-_findNumberLoop:
-    mov   bl, [readBuffer+ecx]
-    cmp   bl, '9'
-    jg   _endNumberLoop
-    sub   bl, '0'
-    cmp   bl, 0
-    jl    _endNumberLoop
-    mov   edx, 10              ; save multiplier for later need
-    mul   edx
-    add   eax, ebx
-    inc   ecx                   ; Go to next location in number
-    jmp   _findNumberLoop
- _endNumberLoop:
-    cmp    eax,0
-    jle    _rereadMaxNumber
-    cmp    eax, 45
-    jg    _rereadMaxNumber
 
-    pop eax
-    pop ebx
-    push eax
-    push ebx
-    ret
-inputToInteger ENDP
 
 
 ; Convert number to string
@@ -138,6 +84,7 @@ _fillString:
         call writeline
         ret                        ; And return
 writeInt ENDP
+
 writeline PROC near
 _writeline :
 pop		eax; pop the address of the stack into eax
@@ -156,13 +103,17 @@ ret
 writeline ENDP
 
 readline PROC near
-_readline: ; ReadConsole(handle, &readBuffer[0], numCharsToRead, numCharsRead, 0)
+_readline: ; ReadConsole(handle,&readBuffer[0], numCharsToRead, numCharsRead, 0)
+        pop  eax
+        pop  ebx
+        push eax
         push 0
         push offset numCharsRead
         push 255
-        push offset readBuffer
+        push ebx
         push inputHandle
         call _ReadConsoleA@20
+        
         ret
 readline ENDP
 
