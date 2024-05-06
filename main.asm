9d8a988595d954f81c385cd48d05d91baf51d3d0
Dunne-Andrew <71160402+Dunne-Andrew@users.noreply.github.com>
2024-05-05 22:23:37 -0600

    Add files via upload

--- main.asm
+++ main.asm
@@ -2,17 +2,38 @@
 .model flat
 
 
-extern   initUtilities:proc
+extern	 initUtilities:proc
+
+extern	 inputStringParser:proc
+extern	 calculate:proc
+extern	 writeInt:proc
+extern	 writeline:proc
+; extern	 readline:proc
+
 extern   _ExitProcess@4: near
 
 
 .data
+	result              byte        'The answer is: '
+	reset				byte		10,10
 
 .code
 main PROC near
 _main:
 
 	call initUtilities
+	call inputStringParser
+	call calculate
+	; pop eax
+	push 15
+    push offset result
+    call writeline
+	; push eax
+	call writeInt
+	push 2
+	push offset reset
+	call writeline
+	jmp _main
 
 	push	0
 	call	_ExitProcess@4
